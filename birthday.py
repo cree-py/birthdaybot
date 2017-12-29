c014f381810521e8455dc73f0a048cdbabcdbb23
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2017-12-29 17:59:56 -0600

    Create birthday.py

---
+++ birthday.py
@@ -0,0 +1,91 @@
+import discord
+from discord.ext import commands
+import json as jason
+
+bot = commands.Bot(command_prefix='b!', description='A bot for managing birthdays!')
+
+@bot.event
+async def on_ready():
+    print("Bot Is Online.")
+
+# on_guild_join is modified from CreeperBot
+@bot.event
+async def on_guild_join(guild):
+    # Loop through all channels until a good channel is found. Implemented due to removal of default channel
+    success = False
+    index = 0
+    while not success:
+        try:
+            await guild.channels[index].send("Hello! I'm BirthdayBot, a bot for tracking your birthdays! Get started with `b!help` and `b!setbirthday`.")
+        except discord.Forbidden:
+            index += 1
+        except AttributeError:
+            index += 1
+        except IndexError:
+            # if the server has no channels, doesn't let the bot talk, or all vc/categories
+            pass
+        else:
+            success = True
+            
+@bot.command()
+async def ping(ctx):
+    '''Check if bot is working.'''
+    await ctx.send("Pong. I work.")
+    
+@bot.command()
+async def setbirthday(ctx):
+    '''Set a birthday.'''
+    member = ctx.message.author.id
+    await ctx.send("What is your birthday? Please use MM/DD format.")
+    def check(user):
+        return user == ctx.message.author and user == ctx.message.channel
+    msg = await bot.wait_for('message', check=check)
+    try:
+        list = msg.split("/")
+        if list[0] > 13 or list[0] < 1:
+            await ctx.send("Invalid date.")
+            await ctx.send("Aborting...")
+            return
+        else:
+            pass
+            
+        if list[0] in (1, 3, 5, 7, 8, 10, 12):
+            if list[1] > 31 or list[1] < 1:
+                await ctx.send("Invalid date.")
+                await ctx.send("Aborting...")
+                return
+            else:
+                pass
+        elif list[0] in (4, 6, 9, 11):
+            if list[1] > 30 or list[1] < 1:
+                await ctx.send("Invalid date.")
+                await ctx.send("Aborting...")
+                return
+            else:
+                pass
+        elif list[0] == 2:
+            if list[1] > 29 or list[1] < 1:
+                await ctx.send("Invalid date.")
+                await ctx.send("Aborting...")
+                return
+            else:
+                pass
+        else:
+            await ctx.send("Invalid date.")
+            await ctx.send("Aborting...")
+            return
+    except:
+        await ctx.send("Invalid date.")
+        await ctx.send("Aborting...")
+        return
+    
+    list = msg.split("/")
+    month = list[0]
+    day = list[1]
+    
+    with open('./birthdays.json', 'r+') as f:
+        config = json.load(f)
+        f.seek(0)
+        config[member]['month'] = [month]
+        config[member]['day'] = [day]
+        json.dump(config, f, indent=4)
